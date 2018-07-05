6025461bcc59fb0b8427c49381a7de755f3742bf
monkeyfists <monkeyfistr@protonmail.com>
2018-07-05 00:16:37 -0400

    modified:   README.md 	new file:   bg.jpg 	modified:   config.js 	modified:   config.php 	new file:   css/themes/mtip/bootstrap.css 	new file:   css/themes/mtip/style.css 	modified:   css/themes/turtle/bootstrap.css 	modified:   css/themes/turtle/style.css 	deleted:    favicon.ico 	new file:   img/bg.jpg 	deleted:    img/icon_dark.png 	deleted:    img/icon_light.png 	deleted:    img/turtle_shield.svg 	modified:   index.html 	new file:   mtip/bootstrap.css 	new file:   mtip/style.css 	new file:   themes/mtip/bootstrap.css 	new file:   themes/mtip/style.css 	new file:   themes/turtle/bootstrap.css 	new file:   themes/turtle/style.css 	new file:   turtle/bootstrap.css 	new file:   turtle/style.css

--- config.js
+++ config.js
@@ -1,28 +1,18 @@
-var api = 'http://127.0.0.1:11898';
+var api = 'http://149.28.102.229:13002';
 var donationAddress = "";
-var blockTargetInterval = 30;
+var blockTargetInterval = 40;
 var coinUnits = 100;
-var symbol = 'trtl';
+var symbol = 'mtip';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
- "trtl": [
-	["z-pool.com", "http://z-pool.com:8117"],
-	["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
-	["us.turtlepool.space", "http://us.turtlepool.space:8117"],
-	["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
-	["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
-	["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-	["trtl.heigh-ho.funkypenguin.co.nz", "https://api.trtl.heigh-ho.funkypenguin.co.nz"],
-	["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
-	["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-	["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	["xk.is", "https://xk.is/api"]
+ "mtip": [
+	["mtip.wahaobi.com", "http://mtip.wahaobi.com/:8121"]
  ]
 };
 
 var networkStat2 = {
-    "trtl": [
+    "mtip": [
 	[""]
  ]
 };
